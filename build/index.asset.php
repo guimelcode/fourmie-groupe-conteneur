034ae796fd11f3ac116a24a54bb3bc83d0aa48a6
Guillaume GuiMel Melennec <guillaume.melennec@gmail.com>
2022-04-12 16:43:35 +0200

    Update build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c12efff764a893d56d6742f0af348cfa');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '138e059c2d574346458dcaebe45d6b0d');
